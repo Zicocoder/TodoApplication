c0b110d8a18428a9ffe1d70ac1b1810b813c65a4
ZicoCoder <Azzoug.zackaria@gmail.com>
2025-11-03 23:54:46 +0100

    Created TodoItem and made a test on main. Worked flawlessly.

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,12 +1,14 @@
-package se.lexicon;
-
+import java.time.LocalDate;
 import se.lexicon.model.Person;
+import se.lexicon.model.TodoItem;
+
 
 public class Main {
     public static void main(String[] args) {
-
-
         Person p1 = new Person(1, "Nisse", "Olsson", "nisse@gmail.com");
-        System.out.println(p1.getSummary());
+        TodoItem item = new TodoItem(101, "Change tires", "Winter is coming", LocalDate.of(2025, 12, 1), p1);
+
+        System.out.println(item.getSummary());
+        System.out.println("Is overdue? " + item.isOverdue());
     }
 }
--- src/main/java/se/lexicon/model/TodoItem.java
+++ src/main/java/se/lexicon/model/TodoItem.java
@@ -1,4 +1,88 @@
 package se.lexicon.model;
 
+import java.time.LocalDate;
+
 public class TodoItem {
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate deadLine;
+    private boolean done;
+    private Person creator;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Person getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Person creator) {
+        this.creator = creator;
+    }
+
+    public TodoItem(int id, String title, String description, LocalDate deadLine, Person creator) {
+        if (title == null || title.isEmpty() || deadLine == null) {
+            throw new IllegalArgumentException("Title and deadline cannot be null or empty");
+        }
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.deadLine = deadLine;
+        this.creator = creator;
+        this.done = false;
+
+    }
+
+    public boolean isOverdue(){
+
+
+        return LocalDate.now().isAfter(deadLine);
+
+    }
+    public String getSummary() {
+        return "{id " + id +
+                ", done: " + done +
+                ", deadline " + deadLine +
+                ", creator: " + (creator != null ? creator.getFirstName() + " " + creator.getLastName() : "none") +
+                "}";
+    }
+
 }
