021e5357df336bcf9491d778689383adce5f9dd1
ZicoCoder <Azzoug.zackaria@gmail.com>
2025-11-03 22:13:04 +0100

    Made classes, and finished Person.java

---
+++ src/main/java/se/lexicon/model/Person.java
@@ -0,0 +1,59 @@
+package se.lexicon.model;
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    public Person(int id, String firstName, String lastName, String email) {
+        if (firstName == null || lastName == lastName || email == null) {
+            throw new IllegalArgumentException("Fields cannot be null");
+        }
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSummary() {
+        return "(id: " + id +
+                ", name " + firstName + " " + lastName +
+                ", email; " + email + "}";
+    }
+
+}
