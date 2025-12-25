e1244a7bcc9b0423945281246889b4d2246d4cff
ZicoCoder <Azzoug.zackaria@gmail.com>
2025-12-25 14:32:59 +0100

    added queries to Create person, find by id and find all

---
+++ src/main/java/se/lexicon/dao/PeopleImpl.java
@@ -0,0 +1,146 @@
+package se.lexicon.dao;
+import se.lexicon.model.Person;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class PeopleImpl implements People{
+
+    private final Connection connection;
+
+    public PeopleImpl(Connection connection) {
+        this.connection = connection;
+
+    }
+
+
+    @Override
+    public Person create(Person person) {
+        if (person == null) {
+            throw new IllegalArgumentException("person cant be null");
+
+        }
+        String sql = "INSERT INTO person (first_name, last_name) VALUES (?, ?)";
+
+        try (
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+
+            preparedStatement.setString(1, person.getFirstName());
+            preparedStatement.setString(2, person.getLastName());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows > 0) {
+                try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                    if (resultSet.next()) {
+                        person.setPersonId((resultSet.getInt(1) ));
+
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return person;
+    }
+
+
+
+    @Override
+    public Person findById(int id) {
+        String sql = "SELECT * FROM person WHERE person_id = ?";
+
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+                ) {
+            preparedStatement.setInt(1, id);
+
+            try (
+                    ResultSet resultSet = preparedStatement.executeQuery()
+                    ) {
+                if (resultSet.next()) {
+                    return new Person(
+                            resultSet.getInt("person_id"),
+                            resultSet.getString("first_name"),
+                            resultSet.getString("last_name")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<Person> findAll() {
+        java.util.List<Person> people = new java.util.ArrayList<>();
+
+        String sql = "SELECT * FROM person";
+
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            while (resultSet.next()) {
+                Person person = new Person(
+                        resultSet.getInt("person_id"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name")
+                );
+                people.add(person);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return people;
+    }
+
+    @Override
+    public Collection<Person> findByName(String name) {
+        java.util.List<Person> people = new java.util.ArrayList<>();
+
+        String sql = "SELECT * FROM person where first_name LIKE ? OR last_name LIKE ?";
+
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            String pattern = "%" + name + "%";
+                preparedStatement.setString(1, pattern);
+                preparedStatement.setString(2, pattern);
+
+               try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+
+            while (resultSet.next()) {
+                people.add(new Person(
+                        resultSet.getInt("person_id"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name")
+                ));
+
+            }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return people;
+    }
+
+    @Override
+    public Person update(Person person) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
